1e3350aae7cf4c8952ebee9dc53f39d57a2e1466
Saranya2003 <62011240@KMITL.AC.TH>
2021-11-12 17:01:29 +0700

    fix code

--- Plagiarism Analyzer GUI.py
+++ Plagiarism Analyzer GUI.py
@@ -2,47 +2,48 @@ import os
 #from pyswip import Prolog
 from tkinter import *
 from tkinter.ttk import *
+from tkinter import filedialog
 from tkinter.filedialog import askopenfile
 
-original=[doc for doc in os.listdir() if doc.endswith('.txt')]
-sample=[open(_file,encoding='utf-8').read() for _file in original]
-
-#vector=vectorize()
-s_vector=list(zip)
 
 def openFile1():
-    tf = askopenfile(
-        initialdir="C:/Users/Saranya Prasertsang/Desktop/", 
+    tf = filedialog.askopenfilename(
+        initialdir="C:/Users/MainFrame/Desktop/", 
         title="Open Text file", 
         filetypes=(("Text Files", "*.txt"),)
         )
     pathh1.insert(END, tf)
-    tf = open(tf,'r')
+    tf = open(tf)  # or tf = open(tf, 'r')
     data = tf.read()
     txtarea1.insert(END, data)
     tf.close()
+    
 def openFile2():
-    tf = askopenfile(
-        initialdir="C:/Users/Saranya Prasertsang/Desktop/", 
+    tf = filedialog.askopenfilename(
+        initialdir="C:/Users/MainFrame/Desktop/", 
         title="Open Text file", 
         filetypes=(("Text Files", "*.txt"),)
         )
     pathh2.insert(END, tf)
-    tf = open(tf,'r')
+    tf = open(tf)  # or tf = open(tf, 'r')
     data = tf.read()
     txtarea2.insert(END, data)
     tf.close()
+
 def checkPlagiarism():
     result= set()
     global s_vectors
 
 ws = Tk()
 ws.title("Plagiarism Analyzer")
-ws.resizable(False,False)
+ws.geometry("400x600")
 
 ws['bg']='#C63785'
 
-txtarea1 = Text(ws, width=20, height=20)
+title=Label(text="Please open a file to check plagiarism.")
+title.pack()
+
+txtarea1 = Text(ws,height=10)
 txtarea1.pack()
 
 pathh1 = Entry(ws)
@@ -50,7 +51,7 @@ pathh1.pack()
 
 loadfilebtn1 = Button(ws, text="Open File 1", command=openFile1).pack()
 
-txtarea2 = Text(ws, width=20, height=20)
+txtarea2 = Text(ws,height=10)
 txtarea2.pack()
 
 pathh2 = Entry(ws)
@@ -59,7 +60,7 @@ pathh2.pack()
 loadfilebtn2 = Button(ws, text="Open File 2", command=openFile2).pack()
 
 plagiarismCheckbtn = Button(ws, text="Check", command=checkPlagiarism).pack()
-showresult = Text(ws, width=20, height=20)
+showresult = Text(ws,width=10, height=5)
 showresult.pack()
 ws.mainloop()
 
