9881036bd24762ac5bb19188dadef22ca41a4fa6
Saranya2003 <62011240@KMITL.AC.TH>
2021-12-11 14:08:36 +0700

    Change Python code + Add new sample files

--- Plagiarism Analyzer GUI.py
+++ Plagiarism Analyzer GUI.py
@@ -4,59 +4,55 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import filedialog
 from tkinter.filedialog import askopenfile
-from numpy import vectorize
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from difflib import SequenceMatcher
 
-#Define file directory and attribute in plagiarism checking function
-files = [doc for doc in os.listdir() if doc.endswith('.txt')]
-contents = [open(File).read() for File in files]
-vectorize = lambda Text: TfidfVectorizer().fit_transform(Text).toarray()
 
-similarity = lambda doc1, doc2: cosine_similarity([doc1,doc2])
 
-vectors = vectorize(contents)
-s_vectors = list(zip(files,vectors))
+
 #Load prolog file
 prolog = Prolog()
 prolog.consult("Plagiarism Analyzer")
 
 #open file 1
 def openFile1():
+    txtarea1.delete("1.0",END)
     tf = filedialog.askopenfilename(
         initialdir="C:/Users/MainFrame/Desktop/", 
         title="Open Text file", 
         filetypes=(("Text Files", "*.txt"),)
         )
     pathh1.insert(END, tf)
-    tf = open(tf)  # or tf = open(tf, 'r')
-    data = tf.read()
-    txtarea1.insert(END, data)
-    tf.close()
+    data=""
+    with open(tf) as f:
+        for line in f:
+            data+=line
+        txtarea1.insert(END,data)
+    return data
+
 #open file 2   
 def openFile2():
+    txtarea2.delete("1.0",END)
     tf = filedialog.askopenfilename(
         initialdir="C:/Users/MainFrame/Desktop/", 
         title="Open Text file", 
         filetypes=(("Text Files", "*.txt"),)
         )
     pathh2.insert(END, tf)
-    tf = open(tf)  # or tf = open(tf, 'r')
-    data = tf.read()
-    txtarea2.insert(END, data)
-    tf.close()
+    data=""
+    with open(tf) as f:
+        for line in f:
+            data+=line
+        txtarea2.insert(END,data)
+    return data
+
 #Check plagiarism
 def checkPlagiarism():
-    global s_vectors
-    for file_a, text_a in s_vectors:
-        new_vectors = s_vectors.copy()
-        current_index = new_vectors.index((file_a,text_a))
-        del new_vectors[current_index]
-        for file_b, text_b in new_vectors:
-            sim_score = similarity(text_a,text_b)[0][1]
-            sim_score = "%.2f" %sim_score
-            score = "Plagiarize result: "+str(sim_score)
-    showresult.config(text=score)
+    doc1=txtarea1.get("1.0",END)
+    doc2=txtarea2.get("1.0",END)
+    result = SequenceMatcher(None,doc1,doc2).ratio()
+    result = "%.2f"%(result)
+    plag_result = "Plagiarism Score : "+str(result)
+    showresult.config(text=plag_result)
 
 #GUI for the program
 ws = Tk()
